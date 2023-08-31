7826bd193608101061ebfa3aa91dc6ef7d702bef
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-08-31 19:35:26 +0200

    cleaning code and adding a  {0} replacement code to host the message, in this case

--- Program.cs
+++ Program.cs
@@ -36,9 +36,13 @@
             string userValue = Console.ReadLine();
 
             string message = (userValue == "1") ? "boat" : "strand of lint";
-            Console.Write("You won a ");
-            Console.Write(message);
-            Console.Write(".");
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            Console.WriteLine("You won a {0}.", message);
+
             Console.ReadLine();
         }
     }
