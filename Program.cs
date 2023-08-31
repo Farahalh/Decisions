ea08d5a93043d980d90bbf7090b38b0dd136de64
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-08-31 19:28:16 +0200

    exploring the conditional operator
    useful for if else condition, not if you have multiple conditions

--- Program.cs
+++ Program.cs
@@ -31,6 +31,15 @@
             Console.ReadLine()
             */;
 
+            Console.WriteLine("Bob´s Big Giveaway");
+            Console.Write("Choose a door: 1,2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.Write("You won a ");
+            Console.Write(message);
+            Console.Write(".");
+            Console.ReadLine();
         }
     }
 }
